a620b67191fdf80d414e1a666ab6c185df7359ca
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-07-12 11:34:48 -0400

    stopped for help

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ] 
 
+function superbowlWin(element, index, array) {
+    // how do I acccess the object of an element?
+    if  (object.keys(record) === "W") {
+        return (object.key)
+    } else {
+        return "undefined"
+    }
+}
